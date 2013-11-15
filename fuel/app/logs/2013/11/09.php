f9b353e20f5b7e2910490f5f1410aa8cbb4705bc
Miky Dev <mikyondev@gmail.com>
2013-11-15 16:04:03 +0100

    remove logs

--- fuel/app/logs/2013/11/09.php
+++
@@ -1,8 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-INFO - 2013-11-09 22:23:10 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "default/oauth"
-INFO - 2013-11-09 22:23:10 --> Fuel\Core\Request::execute - Called
-INFO - 2013-11-09 22:23:10 --> Fuel\Core\Request::execute - Setting main Request
-INFO - 2013-11-09 22:23:10 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = ""
-INFO - 2013-11-09 22:23:10 --> Fuel\Core\Request::execute - Called
-INFO - 2013-11-09 22:23:10 --> Fuel\Core\Request::execute - Setting main Request
